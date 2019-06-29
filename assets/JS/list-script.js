40295e3af6f26dc8edd859ca40911261d477b14c
Niles Dyer-Hunter <NXDH927@outlook.com>
2019-06-29 09:43:34 -0600

    Update

---
+++ assets/JS/list-script.js
@@ -0,0 +1,17 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCqM2f9DeOPI0-KeI_egjercfmcre6JWVQ",
+    authDomain: "project-1-db2b6.firebaseapp.com",
+    databaseURL: "https://project-1-db2b6.firebaseio.com",
+    projectId: "project-1-db2b6",
+    storageBucket: "",
+    messagingSenderId: "416821496055",
+    appId: "1:416821496055:web:00871c0b1b76e1b2"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+var firstPlace = 0;
+var secondPlace = 1;
+var thirdPlace = 2;
+var fourthPlace = 3;
